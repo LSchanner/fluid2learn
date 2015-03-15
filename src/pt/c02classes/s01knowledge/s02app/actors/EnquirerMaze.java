ce5664a0ae4ead2964a3b1c30441b52daf42b47d
Lauro Cruz e Souza <laurocruzsouza@gmail.com>
2015-03-15 19:24:19 -0300

    EnquirerMaze feito sem utilizar a função finalAnswer de ResponderMaze

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -8,61 +8,61 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 
 public class EnquirerMaze implements IEnquirer {
 
-	IResponder responder;
-	
-	public void connect(IResponder responder) {
-		this.responder = responder;
-	}
+    IResponder responder;
 
-	public boolean discover() {
-		String[] direcoes = new String[]{"norte", "leste", "sul", "oeste"};
-		Stack<Integer> stack = new Stack<>();
-		int pos = -1, excp = -1, i;
-		// Scanner scanner = new Scanner(System.in);
-		String direction;
-		Boolean cont;
+    public void connect(IResponder responder) {
+        this.responder = responder;
+    }
 
-		String stop = responder.ask("aqui");
+    public boolean discover() {
+        String[] direcoes = new String[]{"norte", "leste", "sul", "oeste"};
+        Stack<Integer> stack = new Stack<>();
+        int pos = -1, excp = -1, i;
+        // Scanner scanner = new Scanner(System.in);
+        String direction;
+        Boolean cont;
 
-		while (!stop.equalsIgnoreCase("saida")) {
+        String stop = responder.ask("aqui");
 
-			cont = true;
-			if (!stack.empty())
-				excp = (stack.peek() + 2) % 4;
+        while (!stop.equalsIgnoreCase("saida")) {
 
-			for (i = pos + 1; i < 4 && cont; i++) {
-				direction = responder.ask(direcoes[i]);
+            cont = true;
+            if (!stack.empty())
+                excp = (stack.peek() + 2) % 4;
+
+            for (i = pos + 1; i < 4 && cont; i++) {
+                direction = responder.ask(direcoes[i]);
                 System.out.print("(P)ergunta, (M)ovimento ou (F)im? "); System.out.println("P");
                 System.out.print("  --> "); System.out.println(direcoes[i]);
                 System.out.println(" Resposta: " + direction);
 
-				if ( ( direction.equalsIgnoreCase("passagem") || direction.equalsIgnoreCase("saida") ) && (i != excp)) {
+                if ( ( direction.equalsIgnoreCase("passagem") || direction.equalsIgnoreCase("saida") ) && (i != excp)) {
                     System.out.print("(P)ergunta, (M)ovimento ou (F)im? "); System.out.println("M");
                     System.out.print("  --> "); System.out.println(direcoes[i]);
                     System.out.println("  Movimento executado!");
-					stack.push(i);
-					pos = -1;
-					responder.move(direcoes[i]);
-					cont = false;
-				}
-			}
+                    stack.push(i);
+                    pos = -1;
+                    responder.move(direcoes[i]);
+                    cont = false;
+                }
+            }
 
-			if (cont && !stack.empty()) {
-				System.out.println("Voltando");
-				pos = stack.peek();
-				responder.move(direcoes[(stack.pop() + 2)%4]);
-			}
+            if (cont && !stack.empty()) {
+                System.out.println("Voltando");
+                pos = stack.peek();
+                responder.move(direcoes[(stack.pop() + 2)%4]);
+            }
 
-			stop = responder.ask("aqui");
+            stop = responder.ask("aqui");
 
-		}
+        }
 
-		if (stop.equalsIgnoreCase("saida")) {
-			System.out.println("ACHEEEEEEEEEEEI");
-			return true;
-		}
-		return false;
+        if (stop.equalsIgnoreCase("saida")) {
+            System.out.println("ACHEEEEEEEEEEEI");
+            return true;
+        }
+        return false;
 
-	}
-	
+    }
+
 }
