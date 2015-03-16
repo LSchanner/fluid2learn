e9c9b619d3fa42efd82cc0f102500094e66ac62f
Lauro Cruz e Souza <laurocruzsouza@gmail.com>
2015-03-16 13:27:37 -0300

    Cria OrchestratorInit

---
+++ src/pt/c02classes/s01knowledge/s02app/app/OrchestratorInit.java
@@ -0,0 +1,61 @@
+package pt.c02classes.s01knowledge.s02app.app;
+
+import pt.c02classes.s01knowledge.s01base.impl.BaseConhecimento;
+import pt.c02classes.s01knowledge.s01base.impl.Statistics;
+import pt.c02classes.s01knowledge.s01base.inter.IBaseConhecimento;
+import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
+import pt.c02classes.s01knowledge.s01base.inter.IResponder;
+import pt.c02classes.s01knowledge.s01base.inter.IStatistics;
+import pt.c02classes.s01knowledge.s02app.actors.EnquirerAnimals;
+import pt.c02classes.s01knowledge.s02app.actors.EnquirerMaze;
+import pt.c02classes.s01knowledge.s02app.actors.ResponderAnimals;
+import pt.c02classes.s01knowledge.s02app.actors.ResponderMaze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class OrchestratorInit
+{
+    public static void main(String[] args)
+    {
+        IEnquirer enq;
+        IResponder resp;
+        IStatistics stat;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Você quer jogar um jogo? [y|n]");
+        in.nextLine();
+        System.out.println("Ok, então. Animals ou Maze?");
+        String s = in.nextLine();
+
+        if(s.equalsIgnoreCase("animals")){
+            IBaseConhecimento base = new BaseConhecimento();
+
+            base.setScenario("animals");
+            String listaAnimais[] = base.listaNomes();
+            for (int animal = 0; animal < listaAnimais.length; animal++) {
+                System.out.println("Enquirer com " + listaAnimais[animal] + "...");
+                stat = new Statistics();
+                resp = new ResponderAnimals(stat, listaAnimais[animal]);
+                enq = new EnquirerAnimals();
+                enq.connect(resp);
+                enq.discover();
+                System.out.println("----------------------------------------------------------------------------------------\n");
+            }
+        }else if(s.equalsIgnoreCase("maze")) {
+            System.out.print("Qual labirinto? ");
+            String mapa = in.nextLine();
+
+            if (mapa.equalsIgnoreCase("mordor")) {
+                System.out.println("Enquirer com Mordor...");
+                stat = new Statistics();
+                resp = new ResponderMaze(stat, "mordor");
+                enq = new EnquirerMaze();
+                enq.connect(resp);
+                enq.discover();
+            } else System.out.println("Mapa não existe");
+            System.out.println("----------------------------------------------------------------------------------------\n");
+        }
+    }
+}
